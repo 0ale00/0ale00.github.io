039495e0d131afd3322b1cfa1d3ab5c8b139ca12
0ale00 <alessiagrande@hotmail.it>
2023-01-13 12:48:09 +0100

    up

---
+++ lezione21/refactor/js/function.js
@@ -0,0 +1,82 @@
+class Person {
+    constructor(name, age, city = "", email = "") {
+      this.name = name;
+      this.age = age;
+      this.city = city;
+      this.email = email;
+    }
+  }
+  
+  const populatePersonsList = function (persons) {
+    function toListItem({ name, age, city, email }) {
+      return `<li class="list-group-item d-flex">
+                   <div>${name} ${city ? `(${city})` : ""} ${
+        email ? `mail: ${email}` : ""
+      }</div>
+                   <div class="ms-auto">${age} anni</div>
+                </li>`;
+    }
+  
+    if (persons instanceof Array) {
+      const ul = document.getElementById("persons-list");
+      ul.innerHTML = persons
+        .filter((p) => p instanceof Person)
+        .sort((a, b) => a.age - b.age)
+        .map(toListItem)
+        .join("");
+    }
+  };
+  
+  const populateOptions = function (persons) {
+    if (persons instanceof Array) {
+      const form = document.forms.item(1);
+      const select = form.elements[0];
+      //Ordino alfabetico
+      const newOptions = persons
+        .filter((p) => p instanceof Person)
+        .sort((a, b) => {
+          const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        })
+        .map((p, index) => new Option(p.name, index.toString()));
+      const firstChild = [select.children[0]];
+      const t = firstChild.concat(newOptions);
+      select.innerHTML = ""; // svuota semplice
+      t.forEach((i) => {
+        select.append(i);
+      });
+    }
+  };
+  
+  const persons = {
+      list: [
+        new Person("Zlaus", 82),
+        new Person("Dario", 81, "Madrid"),
+        new Person("Calter", 45, "Hamburg"),
+        new Person("Busi", 39, "Hamburg", "susi@lego.it"),
+        new Person("Antonio", 21, "Milano"),
+        new Person("Euido", 37, "Torino"),
+      ],
+      addItemToList: function (item) {
+        this.list.push(item);
+        document.dispatchEvent(
+          new Event("listUpdated", {
+            bubbles: true,
+          })
+        );
+      },
+      getList: function () {
+        return [...this.list];
+      },
+      somethingElse: "string",
+    };
+  
+  export default populatePersonsList;
+  export { Person, populateOptions,persons };
---
+++ lezione21/refactor/main.js
@@ -0,0 +1,55 @@
+import populatePersonsList, {
+    populateOptions,
+    Person,
+    persons,
+  } from "./js/functions.js";
+  
+  populatePersonsList(persons.list);
+  populateOptions(persons.list);
+  
+  const personCreateForm = document.forms.item(0);
+  personCreateForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const form = e.target;
+    const name = form.elements[0].value;
+    const age = form.elements[1].value;
+    const city = form.elements[2].value;
+    const person = new Person(name, age, city);
+    persons.addItemToList(person);
+    form.reset();
+  });
+  
+  const birthdayForm = document.forms[1];
+  birthdayForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const selectedPersonName = e.target.elements[0].value;
+    const selectedPerson = persons.list
+      .filter((p) => p instanceof Person)
+      .sort((a, b) => {
+        const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      })[selectedPersonName];
+   
+    if (selectedPerson instanceof Person) {
+      selectedPerson.age++;
+      document.dispatchEvent(
+        new Event("listUpdated", {
+          bubbles: true,
+        })
+      );
+    }
+  });
+  
+  document.addEventListener("listUpdated", () => {
+    populatePersonsList(persons.list);
+    populateOptions(persons.list);
+  });
