17ea54b780548100abfb4e8e5dc1e498e0b7931f
0ale00 <alessiagrande@hotmail.it>
2023-01-17 10:28:31 +0100

    ts

--- lezione23/altriesempi.js
+++ lezione23/altriesempi.js
@@ -1,2 +1,21 @@
-var obj = { x: 0 };
-console.log(obj);
+let user: any = { userName: "Mario", userBirthYear: 1980 }; //recuperato da una chiamata
+
+/**
+ * Greet the user and show years
+ * @param name 
+ * @param age 
+ */
+function greet(name: string, age: number): void {
+  console.log("Hello, " + name.toUpperCase() + " hai " + age + " anni.");
+}
+
+/**
+ * Get the age
+ * @param birthYear 
+ * @returns 
+ */
+function getAge(birthYear: number): number {
+  return new Date().getFullYear() - birthYear;
+}
+
+greet(user["userName"], getAge(user["userBirthYear"]));
--- lezione23/altriesempi.ts
+++ lezione23/altriesempi.ts
@@ -1,2 +1,20 @@
-let obj: any ={ x:0 };
-console.log(obj)
+let obj: any = { userName : "Mario", userBirthYear : 1980 };
+// recuperato da chiamata
+
+
+function greet(name: string, age: number) {
+    console.log("Hello, " + name.toUpperCase() + " hai "+age+" anni.");
+  }
+/**
+ * 
+ * @param birthYear 
+ * @returns 
+ */
+
+  function greAge (birthYear: number) : number{
+    return new Date() -birthYear;
+  }
+
+console.log(obj);
+
+greet(user["userName"],getAge(user["userBirthYear"]));
