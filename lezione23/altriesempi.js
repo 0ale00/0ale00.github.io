c864669d7779e27f850014790770d13adaac51ee
0ale00 <alessiagrande@hotmail.it>
2023-01-17 12:35:54 +0100

    type

--- lezione23/altriesempi.js
+++ lezione23/altriesempi.js
@@ -1,21 +1,81 @@
-let user: any = { userName: "Mario", userBirthYear: 1980 }; //recuperato da una chiamata
-
+var user = { userName: "Mario", userBirthYear: 1980 }; //recuperato da una chiamata
 /**
  * Greet the user and show years
- * @param name 
- * @param age 
+ * @param name
+ * @param age
  */
-function greet(name: string, age: number): void {
-  console.log("Hello, " + name.toUpperCase() + " hai " + age + " anni.");
+function greet(name, age) {
+    console.log("Hello, " + name.toUpperCase() + " hai " + age + " anni.");
 }
-
 /**
  * Get the age
- * @param birthYear 
- * @returns 
+ * @param birthYear
+ * @returns
  */
-function getAge(birthYear: number): number {
-  return new Date().getFullYear() - birthYear;
+function getAge(birthYear) {
+    return new Date().getFullYear() - birthYear;
 }
-
 greet(user["userName"], getAge(user["userBirthYear"]));
+var names = ["Alice", "Bob", "Eve"];
+// Contextual typing for function
+names.forEach(function (s) {
+    if (typeof s === "string") {
+        console.log("stringa " + s.toUpperCase);
+    }
+    console.log(s);
+});
+/**
+ * Print coordinates
+ * @param pt
+ */
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+/**
+ * printName and lastName (if provided : last is not mandatory)
+ * @param person
+ */
+function printName(person) {
+    console.log("Name: " + person.first);
+    if (typeof person.last !== "undefined") {
+        console.log("Surname: " + person.last);
+    }
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+/**
+ * Una funzione che chiama un'altra funzione
+ * @param fn
+ */
+function greeter(fn) {
+    fn("Hello, World");
+}
+function printToConsole(s) {
+    console.log(s);
+}
+greeter(printToConsole);
+var optionalCity = null;
+var optionalCountry = undefined;
+console.log("City " + optionalCity + " country " + optionalCountry);
+if (optionalCity === null) {
+    optionalCity = "Roma";
+}
+if (optionalCountry === undefined) {
+    optionalCountry = "Italia";
+}
+console.log("City " + optionalCity + " country " + optionalCountry);
+/**
+ *
+ * @param a
+ * @param b
+ * @returns
+ */
+function bar(a, b) {
+    if (b === void 0) { b = 1; }
+    return a + b;
+}
+console.log(bar(1)); // OK, second argument considered optional
+console.log(bar(1, 5));
--- lezione23/altriesempi.ts
+++ lezione23/altriesempi.ts
@@ -1,20 +1,96 @@
-let obj: any = { userName : "Mario", userBirthYear : 1980 };
-// recuperato da chiamata
+let user: any = { userName: "Mario", userBirthYear: 1980 }; //recuperato da una chiamata
 
+/**
+ * Greet the user and show years
+ * @param name
+ * @param age
+ */
+function greet(name: string, age: number): void {
+  console.log("Hello, " + name.toUpperCase() + " hai " + age + " anni.");
+}
 
-function greet(name: string, age: number) {
-    console.log("Hello, " + name.toUpperCase() + " hai "+age+" anni.");
+/**
+ * Get the age
+ * @param birthYear
+ * @returns
+ */
+function getAge(birthYear: number): number {
+  return new Date().getFullYear() - birthYear;
+}
+
+greet(user["userName"], getAge(user["userBirthYear"]));
+
+const names: string[] = ["Alice", "Bob", "Eve"];
+
+// Contextual typing for function
+names.forEach(function (s) {
+  if (typeof s === "string") {
+    console.log("stringa " + s.toUpperCase);
   }
+  console.log(s);
+});
+
+/**
+ * Print coordinates
+ * @param pt
+ */
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+
+/**
+ * printName and lastName (if provided : last is not mandatory)
+ * @param person
+ */
+function printName(person: { first: string; last?: string }): void {
+  console.log("Name: " + person.first);
+  if (typeof person.last !== "undefined") {
+    console.log("Surname: " + person.last);
+  }
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+
+/**
+ * Una funzione che chiama un'altra funzione
+ * @param fn
+ */
+function greeter(fn: (a: string) => void) {
+  fn("Hello, World");
+}
+
+function printToConsole(s: string) {
+  console.log(s);
+}
+
+greeter(printToConsole);
+
+let optionalCity: string | null = null;
+let optionalCountry: string | undefined = undefined;
+
+console.log("City " + optionalCity + " country " + optionalCountry);
+
+if (optionalCity === null) {
+  optionalCity = "Roma";
+}
+if (optionalCountry === undefined) {
+  optionalCountry = "Italia";
+}
+
+console.log("City " + optionalCity + " country " + optionalCountry);
+
 /**
  * 
- * @param birthYear 
+ * @param a 
+ * @param b 
  * @returns 
  */
+function bar(a : number, b : number = 1) : number {
+  return a+b;
+}
 
-  function greAge (birthYear: number) : number{
-    return new Date() -birthYear;
-  }
-
-console.log(obj);
-
-greet(user["userName"],getAge(user["userBirthYear"]));
+console.log (bar(1)); // OK, second argument considered optional
+console.log (bar(1, 5));
