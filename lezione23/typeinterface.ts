1459cbab0d81dc8768f5e2972ad54b708d11cc7a
0ale00 <alessiagrande@hotmail.it>
2023-01-17 14:22:24 +0100

    ex finale

--- lezione23/typeinterface.ts
+++ lezione23/typeinterface.ts
@@ -95,3 +95,27 @@ interface PersonInterface {
   type linguaggi = "PHP" | "JAVA" | "TYPESCRIPT";
   const myLang: linguaggi = "TYPESCRIPT";
   console.log(myLang);
+
+  class Student {
+    fullName: string;
+    constructor(
+      public firstName: string,
+      public middleInitial: string,
+      public lastName: string
+    ) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+  }
+   
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+   
+  function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+  }
+   
+  let user = new Student("Jane", "M.", "User");
+   
+  document.body.textContent = greeter(user);
